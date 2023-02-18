f49489bf89e59eef7ab7e5e38c97bd3e39809e8c
SamarV-121 <samarvispute121@pm.me>
2023-02-18 14:15:46 +0000

    gms: Enable game dashboard
      * also update files from bluejay TQ1A.230105.001.A2
    
    Change-Id: I3ba2ddc9d24425c5d44e5b0d04bc3d81a8f06e4f

--- common/common-vendor.mk
+++ common/common-vendor.mk
@@ -5,9 +5,13 @@
 PRODUCT_SOONG_NAMESPACES += \
     vendor/gms/common
 
+
+PRODUCT_SOONG_NAMESPACES += vendor/gms/overlay
+PRODUCT_PACKAGES += GmsOverlay 
 PRODUCT_COPY_FILES += \
     vendor/gms/common/proprietary/product/etc/permissions/privapp-permissions-google-p.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-google-p.xml \
     vendor/gms/common/proprietary/product/etc/permissions/split-permissions-google.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/split-permissions-google.xml \
+    vendor/gms/common/proprietary/product/etc/sysconfig/game_service.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/game_service.xml \
     vendor/gms/common/proprietary/product/etc/sysconfig/google.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/google.xml \
     vendor/gms/common/proprietary/product/etc/sysconfig/google_build.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/google_build.xml \
     vendor/gms/common/proprietary/system_ext/etc/permissions/privapp-permissions-google-se.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-google-se.xml
@@ -17,6 +21,3 @@ PRODUCT_PACKAGES += \
     PrebuiltGmsCoreSc \
     AndroidPlatformServices \
     GoogleServicesFramework
-
-PRODUCT_SOONG_NAMESPACES += vendor/gms/overlay
-PRODUCT_PACKAGES += GmsOverlay
